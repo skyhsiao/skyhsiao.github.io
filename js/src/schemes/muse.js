a1705b370f4cedd66423d90d3fdadaa0e8363697
skyhsiao <skyxsky@gmail.com>
2019-03-22 15:46:49 +0800

    Site updated: 2019-03-22 15:46:49

---
+++ js/src/schemes/muse.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){function o(){var o=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(!0):$(".container").height();o<window.innerHeight?$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed"):$("#footer").removeAttr("style position")}o(),$(window).on("resize scroll",o)});
